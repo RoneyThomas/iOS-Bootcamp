bb6f3601a30550ddd8b35331a38885581ebe53e4
RoneyThomas <roneythomas6@gmail.com>
2019-06-29 05:34:00 -0400

    Stacklayout tutorial

---
+++ Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Roney Thomas Mannoocheril on 2019-06-29.
+//  Copyright © 2019 Roney Thomas Mannoocheril. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
